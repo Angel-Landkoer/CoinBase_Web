02a4db938246ae959b25e6e009f6301aeb4c2253
Angel Landkoer <angel.landkoer09@gmail.com>
2023-05-08 09:34:08 -0500

    Detectando HTML en Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 
 module.exports = {
@@ -20,5 +21,12 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ]
 }
