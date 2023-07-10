98f51c900fbb1fc206f8d6173aa29855679cc04e
angelLandkoer <angel.landkoer09@gmail.com>
2023-07-10 09:15:37 -0500

    Capturando nodos y creando elementos y agregandoles clases

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import './styles/index.scss'
 
-const app = document.getElementById("app")
+const closeWindowModal = document.createElement("div")
+const textCloseWindowModal = document.createElement("span")
+closeWindowModal.classList.add("windowClose")
+textCloseWindowModal.textContent = "X"
+closeWindowModal.appendChild(textCloseWindowModal)
 
-const action = "Hello People"
+function handleDOMContentLoaded() {
+  const hero = document.querySelector(".hero")
+  const modal = document.querySelector(".modalHide")
+  const main = document.querySelector(".main")
+  const footer = document.querySelector(".footer")
+  const btnLogin = document.querySelector(".btnLogin")
 
-console.log(action)
+  const loginContent = document.createElement("section")
+  loginContent.classList.add("loginContent")
+
+  const containerContextL = document.createElement("section")
+  containerContextL.classList.add("containerContext")
+
+  const formContentL = document.createElement("section")
+  formContentL.classList.add("formContent")
+
+  const loginForm = document.createElement("form")
+  loginForm.classList.add("formData")
+
+  const signUpContent = document.createElement("section")
+  signUpContent.classList.add("signUpContent")
+
+  const containerContextS = document.createElement("section")
+  containerContextS.classList.add("containerContext")
+
+  const formContentS = document.createElement("section")
+  formContentS.classList.add("formContent")
+
+  const signUpForm = document.createElement("form")
+  signUpForm.classList.add("formData")
+
+  document.removeEventListener("DOMContentLoaded", handleDOMContentLoaded);
+}
+
+document.addEventListener("DOMContentLoaded", handleDOMContentLoaded)
